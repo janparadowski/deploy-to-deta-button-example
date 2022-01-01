96bb15152bf9427bf7dbcfe3ffe85102178fa3a3
janparadowski <janparadowski@users.noreply.github.com>
2022-01-01 13:25:38 -0500

    printing os.environ

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#import os
+import os
 from fastapi import FastAPI, Response
 import requests
 
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 async def index(name: str = "World"):
-	y = [x**x for x in range(1,10,2)]
+	y = os.environ
 	z = f"now running some python code {y}"
 	#name = os.getenv("NAME", "world")
 	return f"hello {name}! {z}"
